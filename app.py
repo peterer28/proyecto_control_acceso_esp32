e08069cd92f041513256e36481cc0fd3eb438c0c
Pedro Ibanez <pibanezg@miumg.com.gt>
2023-10-21 14:22:57 -0600

    renamed the database fields

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import psycopg2
 
 app = Flask(__name)
 
-# Database configuration
+# Database configuration (replace with your actual database details)
 db_config = {
     'dbname': 'your_db_name',
     'user': 'your_db_user',
@@ -12,20 +12,20 @@ db_config = {
     'port': 'your_db_port'
 }
 
-def authorize_access(user, key):
+def authorize_access(user_name, access_key):
     # Connect to the PostgreSQL database
     conn = psycopg2.connect(**db_config)
     cursor = conn.cursor()
     
     # Query the user_authorization table
-    cursor.execute("SELECT authorization FROM user_authorization WHERE user = %s AND key = %s", (user, key))
+    cursor.execute("SELECT auth FROM user_authorization WHERE user_name = %s AND acces_key = %s", (user_name, access_key))
     result = cursor.fetchone()
     
     if result:
-        authorization = result[0]
-        if authorization:
+        auth = result[0]
+        if auth:
             # Log the access event
-            cursor.execute("INSERT INTO access_log (user) VALUES (%s)", (user,))
+            cursor.execute("INSERT INTO access_log (user_name) VALUES (%s)", (user_name,))
             conn.commit()
             conn.close()
             return True
@@ -35,11 +35,11 @@ def authorize_access(user, key):
 
 @app.route('/authorize', methods=['GET'])
 def authorize():
-    user = request.args.get('user')
-    key = request.args.get('key')
+    user_name = request.args.get('user_name')
+    access_key = request.args.get('access_key')
     
-    if user and key:
-        if authorize_access(user, key):
+    if user_name and access_key:
+        if authorize_access(user_name, access_key):
             return jsonify({'aut': 'approved', 'access_key': 'xxxxxxxx'})
         else:
             return jsonify({'aut': 'denied'})
